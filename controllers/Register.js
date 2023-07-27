933637dbeacc7b7a7b2083512d3eefc10671f61c
ivanotch <90089378+ivanotch@users.noreply.github.com>
2023-07-27 12:23:22 +0800

    Update Register.js

--- controllers/Register.js
+++ controllers/Register.js
@@ -27,7 +27,7 @@ const handleRegister = (req, res, db, bcrypt) => {
         .then(trx.commit)
         .catch(trx.rollback)
     })
-    .catch(err => console.log(err))
+    .catch(err => console.error())
 }
  
 export {handleRegister}
